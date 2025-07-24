d708abd4f4f18992e8c16e2acd4d26eaf6490642
Linapeter <linapark331@gmail.com>
2025-07-24 14:00:09 -0300

    .

--- src/exercises/exercise1.py
+++ src/exercises/exercise1.py
@@ -5,7 +5,7 @@
 # Ex: with n=10, the multiples are [3,5,6,9] and the sum is 23
 
 
-def multiples_3_and_5(limit: int) -> str:
+def multiples_3_and_5(limit: int) -> int:
     """this function returns the sum of the multiples of 3 and 5 until limit.
 
     Args:
@@ -18,7 +18,7 @@ def multiples_3_and_5(limit: int) -> str:
     for i in range(1, limit + 1):
         if i % 3 == 0 or i % 5 == 0:
             total += i
-    return f"The sum of the multiples is {total}"
+    return total
 
 
 print(multiples_3_and_5(1000))
