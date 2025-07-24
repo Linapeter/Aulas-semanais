09598fd701d171731ef40efcd18cbfbeb88da4be
Linapeter <linapark331@gmail.com>
2025-07-24 10:08:59 -0300

    .

---
+++ src/exerc_cios_semanais/exercise1.py
@@ -0,0 +1,14 @@
+# Week 1
+
+# Exercise 1
+# Execute a code that returns the sum of multiples of 3 and 5 up to the number n
+# Ex: with n=10, the multiples are [3,5,6,9] and the sum is 23
+
+def multiples_3_and_5(limit) -> int:
+    total = 0
+    for i in range(1, limit+1):
+        if i%3 == 0 or i%5 == 0:
+            total += i
+    return f"The sum of the multiples is {total}"
+
+print(multiples_3_and_5(1000))
