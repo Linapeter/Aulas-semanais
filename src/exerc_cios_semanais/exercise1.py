0bdf13e1adf2d4bcc40eb964e98eb709279d2bc5
Linapeter <linapark331@gmail.com>
2025-07-24 10:23:06 -0300

    .

--- src/exerc_cios_semanais/exercise1.py
+++ src/exerc_cios_semanais/exercise1.py
@@ -4,11 +4,21 @@
 # Execute a code that returns the sum of multiples of 3 and 5 up to the number n
 # Ex: with n=10, the multiples are [3,5,6,9] and the sum is 23
 
-def multiples_3_and_5(limit) -> int:
+
+def multiples_3_and_5(limit: int) -> str:
+    """this function returns the sum of the multiples of 3 and 5 until limit.
+
+    Args:
+        limit (int)
+
+    Returns:
+        str
+    """
     total = 0
-    for i in range(1, limit+1):
-        if i%3 == 0 or i%5 == 0:
+    for i in range(1, limit + 1):
+        if i % 3 == 0 or i % 5 == 0:
             total += i
     return f"The sum of the multiples is {total}"
+
 
 print(multiples_3_and_5(1000))
