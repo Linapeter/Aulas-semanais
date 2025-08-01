00acc8a006fe7179629911b08a41c0093d34514f
Linapeter <linapark331@gmail.com>
2025-08-01 17:03:51 -0300

    :)

--- src/exercises/exercise6.py
+++ src/exercises/exercise6.py
@@ -14,9 +14,6 @@ class ListOps():
 
     def __init__(self, objects: list[Any])  -> None:
         """Definition of items from class: list and length of list
-
-        Args:
-            length (int): length of list
         """
         self.objects = objects
 
@@ -27,30 +24,46 @@ class ListOps():
         return f"List = {self.objects}"
 
     def append(self,new_objects: list[Any]) -> list[Any]:
+        """given two lists, add all items in the second list to the end of the first list
+        """
         self.objects += new_objects
         return self.objects
 
-    def concat(self,serie_of_list: list[list[Any]]) -> list[Any]:
-        for item in serie_of_list:
+    def concat(self,series_of_list: list[list[Any]]) -> list[Any]:
+        """given a series of lists, combine all items in all lists into one flattened list
+        """
+        for item in series_of_list:
             self.objects += item
         return self.objects
 
     def filter(self,condition: Callable[[Any],bool])-> list[Any]:
+        """given a predicate and a list, return the list of all items for which predicate(item) is True
+        """
         return [item for item in self.objects if condition(item)]
 
     def length(self, list_len:list[Any]) -> int:
+        """given a list, return the total number of items within it
+        """
         return len(list_len)
 
     def map(self,function_map: Callable[[Any],Any]) -> list[Any]:
+        """given a function and a list, return the list of the results of applying function(item) on all items
+        """
         return [function_map(mapped) for mapped in self.objects]
 
     def foldl(self, function_fold: Callable[[Any,int],Any], acc: int) -> list[Any]: # left to right
+        """given a function, a list, and initial accumulator, fold (reduce) each item into the accumulator from the left
+        """
         return [function_fold(item,acc) for item in self.objects]
 
     def foldr(self, function_fold: Callable[[Any,int],Any], acc: int) -> list[Any]: # right to left
+        """given a function, a list, and an initial accumulator, fold (reduce) each item into the accumulator from the right
+        """
         return [function_fold(item,acc) for item in reversed(self.objects)]
 
     def reverse(self) -> list[Any]:
+        """given a list, return a list with all the original items, but in reversed order
+        """
         return [reversed(self.objects)]
 
 # Tests
